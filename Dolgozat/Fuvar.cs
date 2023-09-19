146097fcf00732b32e7029ba5ba72028b1c58c5f
kun.attila <kun.attila@CAD2-09>
2023-09-19 13:05:21 +0200

    Add project files.

---
+++ Dolgozat/Fuvar.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dolgozat
+{
+
+    internal class Fuvar
+    {
+        private string taxiId;
+        private string startTime;
+        private string length;
+        private string distance;
+        private string price;
+        private string tip;
+        private string payment;
+
+        public string TaxiId { get => taxiId; set => taxiId = value; }
+        public string StartTime { get => startTime; set => startTime = value; }
+        public string Length { get => length; set => length = value; }
+        public string Distance { get => distance; set => distance = value; }
+        public string Price { get => price; set => price = value; }
+        public string Tip { get => tip; set => tip = value; }
+        public string Payment { get => payment; set => payment = value; }
+
+        public Fuvar(string taxiId, string startTime, string length, string distance, string price, string tip, string payment)
+        {
+            this.taxiId = taxiId;
+            this.startTime = startTime;
+            this.length = length;
+            this.distance = distance;
+            this.price = price;
+            this.tip = tip;
+            this.payment = payment;
+        }
+    }
+}
---
+++ Dolgozat/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Dolgozat
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Fuvar> fuvarok = new List<Fuvar>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void ReadData()
+        {
+            fuvarok.Clear();
+            List<string> list = File.ReadAllLines("fuvar.csv").Skip(1).ToList();
+            foreach(string item in list)
+            {
+                string[] data = item.Split(";");
+                fuvarok.Add(new Fuvar(data[0], data[1], data[2], data[3], data[4], data[5], data[6]));
+            }
+            
+        }
+
+        private void Button_Click1(object sender, RoutedEventArgs e)
+        {
+            ReadData();
+            MessageBox.Show(Convert.ToString(fuvarok.Count()));
+        }
+
+        private void Button_Click2(object sender, RoutedEventArgs e)
+        {
+            ReadData();
+            int income = 0;
+            foreach(Fuvar item in fuvarok)
+            {
+                if (item.TaxiId == "6185")
+                {
+                    income += Convert.ToInt32(item.Price) + Convert.ToInt32(item.Tip);
+                }
+            }
+            MessageBox.Show(Convert.ToString(income));
+        }
+
+        private void Button_Click4(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click3(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click5(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click6(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
